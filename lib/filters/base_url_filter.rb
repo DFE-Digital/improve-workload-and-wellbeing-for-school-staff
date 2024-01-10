c468a6ddf01d2b77009526cb5320754b8dba574a
mark-pkgflow <mark@pkgflow.com>
2024-01-10 12:49:13 +0100

    Add workload reduction toolkit page
    Co-authored-by: AbigailMcP <abigail.mcphillips@digital.education.gov.uk>

---
+++ lib/filters/base_url_filter.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require 'nokogiri'
+
+module Nanoc
+  module Filters
+    # Update occurences of 'base_url' in links, scripts, and images
+    class BaseUrl < Nanoc::Filter
+      identifier :base_url
+
+      def run(content, _params = {})
+        base_url = @config[:base_url]
+        doc = Nokogiri::HTML(content)
+
+        # Update href/src attributes for links, scripts, and images
+        doc.css('a[href], img[src], script[src], link[href]').each do |node|
+          attr = node.name == 'a' ? 'href' : 'src'
+          url = node[attr]
+
+          # Prepend base_url to relative paths
+          node[attr] = "#{base_url}/#{url}" if url && !url.start_with?('http', '/')
+        end
+
+        doc.to_html
+      end
+    end
+  end
+end
