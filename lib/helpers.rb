701e0e739763642ef00cad258a8bce215cc315c4
Abigail McPhillips <abigail.mcphillips@gmail.com>
2024-01-18 16:41:10 +0000

    Remove helper and move html into partials, link new page

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -5,4 +5,3 @@ Dir.glob(File.join('./lib', '**', '*.rb')).sort.each { |f| require f }
 use_helper Nanoc::Helpers::Rendering
 use_helper Nanoc::Helpers::LinkTo
 use_helper Nanoc::Helpers::Breadcrumbs
-use_helper AddressWorkloadIssuesHelper
